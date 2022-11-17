4f254de7c7786a8cb163c4ff6eb0bb04f44d2506
imskanand <the.anand3099@gmail.com>
2022-11-17 18:44:44 +0530

    Some Changes and Refactoring in the Introduction side

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -2,23 +2,26 @@ import React from "react";
 
 function Introduction() {
   return (
-    <div className="container mx-auto my-12 flex h-screen max-w-fit items-center justify-items-center space-x-24 space-y-12 p-6 ">
-      <section className="container mx-auto justify-start ">
+    <div className="container my-12 flex h-screen flex-row items-center space-x-6 p-6">
+      <section className="container ml-60 items-center">
         <h1 className="text-3xl">
           I am{" "}
-          <span className="items-start text-yellow-700">
+          <span className="text-yellow-700 hover:font-bold hover:text-yellow-500">
             Full Stack Developer
           </span>
         </h1>
       </section>
-      <section className="container float-right mx-auto w-1/2 items-center space-y-12 font-extrabold">
+      <section className="space-y-12 font-extrabold">
         <h1 className="text-9xl tracking-wider text-yellow-700">
           Hello
-          <span className="animate-pulse text-9xl text-slate-900">.</span>
+          <span className="animate-pulse text-9xl text-white hover:text-green-500">
+            .
+          </span>
         </h1>
-        <br />
-        <span className="mb-12 text-9xl tracking-wider text-white">I am</span>
-        <br />
+        <span className="text-9xl tracking-wider text-white hover:text-green-50">
+          I am
+        </span>
+        <br className="my-12" />
         <span className="text-9xl tracking-wider text-yellow-700">Shubham</span>
       </section>
     </div>
